d3661ca3bc21a8371e622d20afe257d568fb81e1
Taehyung93 <kaustris@gmail.com>
2020-04-28 16:59:30 +0900

    add Naver Map API

--- app/src/main/java/com/powervoice/vlcforiot/MainActivity.java
+++ app/src/main/java/com/powervoice/vlcforiot/MainActivity.java
@@ -1,14 +1,30 @@
-package com.example.vlcforiot;
+package com.powervoice.vlcforiot;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import com.naver.maps.geometry.LatLng;
+import com.naver.maps.map.LocationTrackingMode;
+import com.naver.maps.map.MapFragment;
+import com.naver.maps.map.NaverMap;
+import com.naver.maps.map.OnMapReadyCallback;
+import com.naver.maps.map.UiSettings;
+import com.naver.maps.map.overlay.Marker;
+import com.naver.maps.map.util.FusedLocationSource;
+import com.naver.maps.map.widget.LocationButtonView;
 
 import org.videolan.libvlc.LibVLC;
 import org.videolan.libvlc.Media;
@@ -17,7 +33,7 @@ import org.videolan.libvlc.interfaces.IVLCVout;
 import org.videolan.libvlc.media.VideoView;
 
 import java.util.ArrayList;
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     private VideoView videoView;
     private Button btnPlay;
@@ -27,12 +43,28 @@ public class MainActivity extends AppCompatActivity {
     private MediaPlayer mMediaPlayer = null;
     private String mFilePath;
 
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 1000;
+    private FusedLocationSource locationSource;
+    private NaverMap naverMap;
+    private UiSettings uiSettings;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
 
+        //checkPermission();
+
+        FragmentManager fm = getSupportFragmentManager();
+        MapFragment mapFragment = (MapFragment)fm.findFragmentById(R.id.map_fragment);
+        if (mapFragment == null) {
+            mapFragment = MapFragment.newInstance();
+            fm.beginTransaction().add(R.id.map_fragment, mapFragment).commit();
+        }
+        locationSource = new FusedLocationSource(this, LOCATION_PERMISSION_REQUEST_CODE);
+        //naverMap.setLocationSource(locationSource);
+        mapFragment.getMapAsync(this);
 
 
         videoView = findViewById(R.id.playerView);
@@ -51,9 +83,46 @@ public class MainActivity extends AppCompatActivity {
                     btnPlay.setText("PLAY");
                     releasePlayer();
                 }
-
             }
         });
+
+
+    }
+
+//    public void checkPermission() {
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M)
+//        {
+//            if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+//                requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, LOCATION_PERMISSION_REQUEST_CODE);
+//
+//            }
+//        }
+//    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String[] permissions,  @NonNull int[] grantResults) {
+        if (locationSource.onRequestPermissionsResult(requestCode, permissions, grantResults)) {
+//            if (!locationSource.isActivated()) { // 권한 거부됨
+//                naverMap.setLocationTrackingMode(LocationTrackingMode.None);
+//            }
+            Log.d("sex","kill -9");
+            return;
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    @Override
+    public void onMapReady(@NonNull NaverMap naverMap) {
+        this.naverMap = naverMap;
+        naverMap.setLocationSource(locationSource);
+        naverMap.setLocationTrackingMode(LocationTrackingMode.NoFollow);
+        //uiSettings.setLocationButtonEnabled(true);
+        naverMap.getUiSettings().setLocationButtonEnabled(true);
+        //Marker marker = new Marker();
+        //marker.setPosition(new LatLng(36.763695, 127.281796));
+        //marker.setMap(naverMap);
 
     }
 
